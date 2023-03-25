63c19a87f473259927fddd2e9c265e301bc713eb
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-03-25 21:29:52 +0600

    url mapping using @WebServlet annotation

--- Request-Dispatch-App/src/com/servlet/Servlet1.java
+++ Request-Dispatch-App/src/com/servlet/Servlet1.java
@@ -5,10 +5,13 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+@WebServlet("/login")
 public class Servlet1 extends HttpServlet{
     
 	String email;
