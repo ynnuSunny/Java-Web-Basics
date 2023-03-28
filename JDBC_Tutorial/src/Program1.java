36ec8cdef7f14e14ab2542de3d81247a91f1361d
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-03-28 11:45:57 +0600

    JBDC update data

--- JDBC_Tutorial/src/Program1.java
+++ JDBC_Tutorial/src/Program1.java
@@ -14,17 +14,22 @@ public class Program1 {
 //			System.out.println("Connection Establish");
 		    
 		    //step 3
-		    PreparedStatement ps = conn.prepareStatement("insert into studentInfo values(?,?,?)");
-		    ps.setInt(1, 84);
-		    ps.setString(2, "Sunny Sutradhar");
-		    ps.setString(3, "Dhaka, Bangladeh");
+//		    PreparedStatement ps = conn.prepareStatement("insert into studentInfo values(?,?,?)");
+//		    ps.setInt(1, 85);
+//		    ps.setString(2, "Sunny Sutradhar");
+//		    ps.setString(3, "Dhaka, Bangladeh");
 		    
+		    //updae
+		    PreparedStatement ps = conn.prepareStatement("update studentInfo set name=? , address=? where id=?");
+            ps.setString(1, "Riya");
+ 		    ps.setString(2, "Mohammod Pur, Bangladeh");
+ 		    ps.setInt(3, 85);
 		    
 		    //step 4
 		    int i = 1;
 		    ps.executeUpdate();
 		    if(i==1) {
-		    	System.out.println("Insert Succefully");
+		    	System.out.println("Update Succefully");
 		    }else {
 		    	System.out.println("Not Inserted");
 		    }
