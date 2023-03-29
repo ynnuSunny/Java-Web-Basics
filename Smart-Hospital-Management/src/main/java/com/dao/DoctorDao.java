094502015fa5bb3e4f55426a2ff2b4caba9d94c8
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-03-30 01:18:51 +0600

    Doctor deatils update page data patch complete

--- Smart-Hospital-Management/src/main/java/com/dao/DoctorDao.java
+++ Smart-Hospital-Management/src/main/java/com/dao/DoctorDao.java
@@ -68,5 +68,33 @@ public class DoctorDao {
 		
 		return list;
 	}
+	
+	public Doctor getDoctorById(int id) {
+		Doctor d = null;
+		
+		try {
+			String sql = "select * from doctor where id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ps.executeQuery();
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				d = new Doctor();
+				d.setId(rs.getInt(1));
+				d.setFullName(rs.getString(2));
+			    d.setDob(rs.getString(3));
+				d.setQualification(rs.getString(4));
+				d.setSpecialist(rs.getString(5));
+				d.setEmail(rs.getString(6));
+				d.setMobNo(rs.getString(7));
+				d.setPassword(rs.getString(8));
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return d;
+	}
 
 }
