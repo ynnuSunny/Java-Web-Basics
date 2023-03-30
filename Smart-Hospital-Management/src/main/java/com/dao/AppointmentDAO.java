eca597aba5bf031482ee24946a235be390f51bcf
Sunny Sutradhar <59875796+ynnuSunny@users.noreply.github.com>
2023-03-30 18:47:45 +0600

    Apointment view by user done

--- Smart-Hospital-Management/src/main/java/com/dao/AppointmentDAO.java
+++ Smart-Hospital-Management/src/main/java/com/dao/AppointmentDAO.java
@@ -47,6 +47,41 @@ public class AppointmentDAO {
 
 		return f;
 	}
+	
+	public List<Appointment> getAllAppointmentByLoginUser(int userId) {
+		List<Appointment> list = new ArrayList<Appointment>();
+		Appointment ap = null;
+
+		try {
+
+			String sql = "select * from appointment where user_id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, userId);
+
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				ap = new Appointment();
+				ap.setId(rs.getInt(1));
+				ap.setUserId(rs.getInt(2));
+				ap.setFullName(rs.getString(3));
+				ap.setGender(rs.getString(4));
+				ap.setAge(rs.getString(5));
+				ap.setAppoinDate(rs.getString(6));
+				ap.setEmail(rs.getString(7));
+				ap.setPhNo(rs.getString(8));
+				ap.setDiseases(rs.getString(9));
+				ap.setDoctorId(rs.getInt(10));
+				ap.setAddress(rs.getString(11));
+				ap.setStatus(rs.getString(12));
+				list.add(ap);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 
 	
 }
